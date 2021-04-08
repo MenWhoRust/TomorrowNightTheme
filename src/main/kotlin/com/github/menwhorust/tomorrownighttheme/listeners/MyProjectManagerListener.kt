357207038df275f94ca70dbfd3123fbe0ccd37cb
GitHub Action <action@github.com>
2021-04-08 15:36:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/menwhorust/tomorrownighttheme/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/menwhorust/tomorrownighttheme/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.menwhorust.tomorrownighttheme.listeners
 
+import com.github.menwhorust.tomorrownighttheme.services.MyProjectService
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/menwhorust/tomorrownighttheme/services/MyProjectService.kt
+++ src/main/kotlin/com/github/menwhorust/tomorrownighttheme/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.menwhorust.tomorrownighttheme.services
 
+import com.github.menwhorust.tomorrownighttheme.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
